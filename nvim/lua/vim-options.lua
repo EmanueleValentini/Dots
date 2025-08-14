df79b55da53df87de903bcd0a07ec96e78308293
Emanuele Valentini <emanuele_valentini@hotmail.it>
2025-08-14 04:34:20 +0200

    Add main configs and gitignore

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
